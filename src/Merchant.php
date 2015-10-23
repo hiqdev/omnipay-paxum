383e80684a5c55b0d693fabf2b32041249a594ee
Andrii Vasyliev <sol@hiqdev.com>
2015-10-23 12:25:02 +0000

    php-cs-fixed

--- src/Merchant.php
+++ src/Merchant.php
@@ -6,7 +6,7 @@
  * @link      https://github.com/hiqdev/php-merchant-paxum
  * @package   php-merchant-paxum
  * @license   BSD-3-Clause
- * @copyright Copyright (c) 2015, HiQDev (https://hiqdev.com/)
+ * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
  */
 
 namespace hiqdev\php\merchant\paxum;
@@ -28,14 +28,14 @@ class Merchant extends \hiqdev\php\merchant\Merchant
     public function getInputs()
     {
         return [
-            'item_name'         => $this->description,
-            'business_email'    => $this->purse,
-            'amount'            => $this->total,
-            'currency'          => $this->currency,
-            'finish_url'        => $this->successUrl,
-            'cancel_url'        => $this->failureUrl,
-            'variables'         => $this->variables,
-            'button_type_id'    => 1, /// Pay Now button
+            'item_name'      => $this->description,
+            'business_email' => $this->purse,
+            'amount'         => $this->total,
+            'currency'       => $this->currency,
+            'finish_url'     => $this->successUrl,
+            'cancel_url'     => $this->failureUrl,
+            'variables'      => $this->variables,
+            'button_type_id' => 1, /// Pay Now button
             //'sandbox'         => 'ON',
         ];
     }
@@ -46,24 +46,25 @@ class Merchant extends \hiqdev\php\merchant\Merchant
         if (!$sum) {
             return 'No sum';
         }
-        if ($data['transaction_status'] != 'done') {
+        if ($data['transaction_status'] !== 'done') {
             return 'Not done';
         }
-        $skips = ['key'=>1, 'ip'=>1, 'auth_login'=>1, 'auth_password'=>1];
-        foreach ($data as $k=>$v) {
+        $skips = ['key' => 1, 'ip' => 1, 'auth_login' => 1, 'auth_password' => 1];
+        foreach ($data as $k => $v) {
             if (!$skips[$k]) {
                 $str .= "&$k=$v";
             }
         }
-        if (md5($str) != strtolower($data['key'])) {
+        if (md5($str) !== strtolower($data['key'])) {
             return 'Wrong hash';
         }
-        $this->mset(array(
-            'from'  => $data['buyer_name'].'/'.$data['buyer_username'].'/'.$data['buyer_id'],
-            'txn'   => $data['transaction_id'],
-            'sum'   => $sum,
-            'time'  => date('c', strtotime($data['transaction_date'].' EST')),
-        ));
-        return null;
+        $this->mset([
+            'from' => $data['buyer_name'] . '/' . $data['buyer_username'] . '/' . $data['buyer_id'],
+            'txn'  => $data['transaction_id'],
+            'sum'  => $sum,
+            'time' => date('c', strtotime($data['transaction_date'] . ' EST')),
+        ]);
+
+        return;
     }
 }
